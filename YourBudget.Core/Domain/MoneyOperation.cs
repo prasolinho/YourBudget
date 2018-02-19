17a30b5e9d1b1a02640a922e713c344bffd04159
TP <prasol@o2.pl>
2018-02-19 06:39:41 +0100

    Add properties to MoneyOperation class

--- YourBudget.Core/Domain/MoneyOperation.cs
+++ YourBudget.Core/Domain/MoneyOperation.cs
@@ -1,7 +1,21 @@
+using System;
+
 namespace YourBudget.Core.Domain
 {
     public class MoneyOperation
     {
+        public decimal Amount { get; protected set; }
+        public string Description { get; protected set; }
+        public MoneyOperationCategory Category { get; protected set; }
+        public DateTime OperationDate { get; protected set; }
 
+        // Do jakiego portfela operacja
+        // TODO: Jeśli tak zostawię to dla operacji bankowych muszę zrobić inny typ operacji. Operacje portfelowe mogę przenosić między różne portfele, a bankowych nie.
+        public Wallet Wallet { get; protected set; }
+
+        protected MoneyOperation()
+        {
+            
+        }
     }
 }
