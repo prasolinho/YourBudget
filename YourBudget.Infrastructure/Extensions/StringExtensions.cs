d4dece604f81b730cefc8e6fcba71c25282f0a54
TP <prasol@o2.pl>
2018-03-02 06:32:01 +0100

    Add StringExtensions clasS

---
+++ YourBudget.Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,10 @@
+using System; 
+
+namespace YourBudget.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool Empty(this string value)
+            => string.IsNullOrWhiteSpace(value);
+    }
+}
