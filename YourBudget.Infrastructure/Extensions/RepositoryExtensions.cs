2887bd13dfc919c4d62c41f164536aac4db10058
Tomasz Prasołek <prasol@o2.pl>
2018-03-16 06:32:10 +0100

    Add repository extensions for get user

---
+++ YourBudget.Infrastructure/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using YourBudget.Core.Domain;
+using YourBudget.Core.Repositories;
+
+namespace YourBudget.Infrastructure.Extensions
+{
+    public static class RepositoryExtensions
+    {
+        public static async Task<User> GetOrFailAsync(this IUserRepository userRepository, Guid userId)
+        {
+            var user = await userRepository.GetAsync(userId);
+            if (user == null)
+            {
+                throw new Exception("User doesn't exitst");
+            }
+            return user;
+        }
+    }
+}
